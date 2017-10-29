e43b89ef78be66eb549b01c39b9ca00569072c4e
Scot Hacker <shacker@birdhouse.org>
2017-10-29 00:37:19 -0700

    Starter home view, with postloop

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
+from blog.models import Post
+
+
+def home(request):
+    posts = Post.objects.all()
+    return render(request, "blog/home.html", {'posts': posts})
