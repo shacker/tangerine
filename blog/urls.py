007f19a591ffe8358bc1c8c606688a99abc8efee
Scot Hacker <shacker@birdhouse.org>
2017-11-01 00:31:54 -0700

    Post category view

--- blog/urls.py
+++ blog/urls.py
@@ -5,4 +5,5 @@ from blog import views
 app_name = 'blog'
 urlpatterns = [
     path('<int:year>/<int:month>/<slug:slug>/', views.post_detail, name="post_detail"),
+    path('cat/<str:cat_slug>/', views.category, name="category"),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 
 
-from blog.models import Post
+from blog.models import Category, Post
 
 
 def home(request):
@@ -12,3 +12,9 @@ def home(request):
 def post_detail(request, year, month, slug):
     post = get_object_or_404(Post, created__year=year, created__month=month, slug=slug)
     return render(request, "blog/post_detail.html", {'post': post})
+
+
+def category(request, cat_slug):
+    cat = Category.objects.get(slug=cat_slug)
+    posts = Post.pub.filter(categories__in=[cat, ])
+    return render(request, "blog/category.html", {'category':  cat, 'posts': posts})
