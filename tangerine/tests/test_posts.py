c5924ac08e78b006c55d95995b1f10c1421ca0e9
Scot Hacker <shacker@birdhouse.org>
2017-11-14 01:02:09 -0800

    Test: Uniquely named RelatedLinkGroups

--- tangerine/tests/test_posts.py
+++ tangerine/tests/test_posts.py
@@ -1,6 +1,7 @@
 import pytest
 
-from tangerine.factories import CategoryFactory, PostFactory
+from tangerine.factories import CategoryFactory, PostFactory, RelatedLinkGroupFactory
+from tangerine.models import RelatedLinkGroup
 
 
 @pytest.mark.django_db
@@ -11,3 +12,10 @@ def test_post_in_multiple_cats():
     post.categories.add(cat1)
     post.categories.add(cat2)
     assert post.categories.all().count() == 2
+
+
+@pytest.mark.django_db
+def test_unique_blogroll():
+    # We can create multiple with same slug, but they are overidden each time, so we should end up with 1
+    RelatedLinkGroupFactory.create_batch(5, slug='foo')
+    assert RelatedLinkGroup.objects.filter(slug='foo').count() == 1
