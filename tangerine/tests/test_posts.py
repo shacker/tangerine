69ad2f64b7bb0684e0985cfbc948a1fa4c0d6c78
Scot Hacker <shacker@birdhouse.org>
2017-11-15 23:41:29 -0800

    Add multiple unit and integration tests

--- tangerine/tests/test_posts.py
+++ tangerine/tests/test_posts.py
@@ -1,7 +1,13 @@
 import pytest
 
 from tangerine.factories import CategoryFactory, PostFactory, RelatedLinkGroupFactory
-from tangerine.models import RelatedLinkGroup
+from tangerine.models import RelatedLinkGroup, Post
+
+
+@pytest.fixture()
+def cats_and_posts():
+    CategoryFactory.create_batch(10)
+    PostFactory.create_batch(30)
 
 
 @pytest.mark.django_db
@@ -16,6 +22,20 @@ def test_post_in_multiple_cats():
 
 @pytest.mark.django_db
 def test_unique_blogroll():
-    # We can create multiple with same slug, but they are overidden each time, so we should end up with 1
+    # We can create multiple blogrolls with same slug, but they are overidden each time, so we should end up with 1
     RelatedLinkGroupFactory.create_batch(5, slug='foo')
     assert RelatedLinkGroup.objects.filter(slug='foo').count() == 1
+
+
+@pytest.mark.django_db
+def test_unpublished_post_not_in_qs(cats_and_posts):
+    somepost = PostFactory(published=False)
+    qs = Post.pub.all()
+    assert somepost not in qs
+
+
+@pytest.mark.django_db
+def test_trashed_post_not_in_qs(cats_and_posts):
+    somepost = PostFactory(trashed=True)
+    qs = Post.pub.all()
+    assert somepost not in qs
---
+++ tangerine/tests/test_views.py
@@ -0,0 +1,42 @@
+from django.urls import reverse
+
+from test_plus.test import TestCase
+
+from tangerine.factories import CategoryFactory, PostFactory
+
+
+class BaseUserTestCase(TestCase):
+
+    def setUp(self):
+        self.user1 = self.make_user("user1")
+
+        self.post1 = PostFactory(slug="somepost")
+        cat = CategoryFactory(slug="somecat")
+        self.post1.categories.add(cat)
+
+        self.post2 = PostFactory(slug="someotherpost")  # Has no category
+
+
+class TestPosts(BaseUserTestCase):
+    '''View tests for posts'''
+
+    def test_good_slug_good_view(self):
+        with self.login(username=self.user1.username):
+            url = self.post1.get_absolute_url()
+            response = self.get(url)
+            self.response_200(response)
+
+    def test_bad_slug_404(self):
+        url = reverse('tangerine:post_detail', kwargs={'year': 2002, 'month': 7, 'day': 23, 'slug': "whatever"})
+        response = self.get(url)
+        self.response_404(response)
+
+    def test_post_in_cat_uses_categories_subtemplate(self):
+        url = self.post1.get_absolute_url()
+        response = self.get(url)
+        assert 'tangerine/include/post_detail_categories.html' in [t.name for t in response.templates]
+
+    def test_post_without_categories_does_not_use_categories_subtemplate(self):
+        url = self.post2.get_absolute_url()
+        response = self.get(url)
+        assert 'tangerine/include/post_detail_categories.html' not in [t.name for t in response.templates]
