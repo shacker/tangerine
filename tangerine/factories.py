c6aa1c307090aa48ccd1d3c5a59677c804b31c8a
Scot Hacker <shacker@birdhouse.org>
2017-11-15 23:40:13 -0800

    PostFactory should not set categories - Change start_tangerine command to match

--- tangerine/factories.py
+++ tangerine/factories.py
@@ -40,11 +40,6 @@ class PostFactory(factory.django.DjangoModelFactory):
         self.author = a
 
     @factory.post_generation
-    def set_cats(self, build, extracted, **kwargs):
-        c = Category.objects.all().order_by('?').first()  # Select random category
-        self.categories.add(c)
-
-    @factory.post_generation
     def set_created(self, build, extracted, **kwargs):
         fake = Faker()
         self.created = fake.date_time_this_decade(tzinfo=pytz.UTC)
--- tangerine/management/commands/tangerine_start.py
+++ tangerine/management/commands/tangerine_start.py
@@ -3,23 +3,21 @@ import os
 
 from django.core.management.base import BaseCommand
 from django.core.management import call_command
+from django.contrib.auth import get_user_model
 
 from tangerine.factories import CategoryFactory, PostFactory
 from tangerine.models import Category
-
-from users.factories import UserFactory
-from users.models import User
 
 
 def gen_su():
     # Generate a superuser for local developer, if needed
     username = pwd.getpwuid(os.geteuid()).pw_name
-    if not User.objects.filter(username=username).exists():
+    if not get_user_model().objects.filter(username=username).exists():
         print("Creating a superuser for YOU.")
         fname = input("First name: ")
         lname = input("Last name: ")
         email = input("Email: ")
-        UserFactory(
+        get_user_model().objects.create(
             username=username, is_superuser=True, is_staff=True,
             first_name=fname, last_name=lname, email="{e}".format(u=username, e=email))
         print("Created user {}. Run `./manage.py changepassword` to set your password.".format(username))
@@ -29,22 +27,23 @@ class Command(BaseCommand):
     help = "For use in development only: Create some fake categories and <num_posts> fake posts via FactoryBoy."
 
     def __init__(self):
-        self.num_posts = 50
+        self.num_posts = 10  # Number of fake posts to be created per category.
 
     def handle(self, *args, **options):
 
-        if not User.objects.all():
+        if not get_user_model().objects.all():
             gen_su()  # Create a superuser and make them author of all posts
+
+        author = get_user_model().objects.order_by('?').first()
 
         cats = ['Family', 'Coding', 'Environment', 'Culture', 'Politics', 'Bike', 'Photography', ]
         if not Category.objects.all().count():
-            for c in cats:
-                CategoryFactory.create(title=c)
-            print("{} fake categories created.".format(len(cats)))
-
-        author = User.objects.order_by('?').first()
-        PostFactory.create_batch(self.num_posts, author=author)
-        print("{} fake posts created.".format(self.num_posts))
+            for cat_title in cats:
+                cat = CategoryFactory.create(title=cat_title)
+                for _ in range(5):
+                    p = PostFactory(author=author)
+                    p.categories.add(cat)
+                print("Five fake posts in category {} created.".format(cat_title))
 
         # Set up starter About page and RelatedLinks to work with default template
         call_command('loaddata', 'about')
